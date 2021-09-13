cc8bb70824a587c695b7580116d23b6c866fe159
Arthur Fincham <arthurfincham@me.com>
2021-09-13 17:08:49 +0100

    added helper method to format the day

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,11 @@
 module HomeHelper
+  def day_formatter(date)
+    if date == Date.today.to_s
+      'Today'
+    elsif date == (Date.today + 1).to_s
+      'Tomorrow'
+    else
+      Date.parse(date).strftime("%A")
+    end 
+  end
 end
